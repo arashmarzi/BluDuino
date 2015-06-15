c38d85ab762d1bbd33a365dd914232b9a190aa94
Arash Marzi <arash.marzi@gmail.com>
2015-06-15 18:30:46 -0400

    added dependencies for bluetooth and android widgets

--- src/com/amarzi/bluduino/MainActivity.java
+++ src/com/amarzi/bluduino/MainActivity.java
@@ -1,12 +1,18 @@
 package com.amarzi.bluduino;
 
+import java.util.Set;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
