ad2fa979ff9774b143251f7bda7bf196c2ac64d4
Arash Marzi <arash.marzi@gmail.com>
2015-06-15 01:32:31 -0400

    turn bluetooth intent on/off

--- src/com/amarzi/bluduino/MainActivity.java
+++ src/com/amarzi/bluduino/MainActivity.java
@@ -14,19 +14,38 @@ public class MainActivity extends Activity {
 	private Button scanBtBtn;
 	private TextView data;
 	private BluetoothAdapter bluetoothAdapter;
-	
+	private Set<BluetoothDevice>pairedDevices;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// initialize bluetooth adapter 
+
+		// initialize bluetooth adapter
 		bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		
+
 		// initialize ui widgets
 		getDataBtn = (Button) this.findViewById(R.id.dataBtn);
 		scanBtBtn = (Button) this.findViewById(R.id.scanBtn);
 		data = (TextView) this.findViewById(R.id.dataTV);
+	}
+
+	public void on(View v) {
+		if (!bluetoothAdapter.isEnabled()) {
+			Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(turnOn, 0);
+			Toast.makeText(getApplicationContext(), "Turned on",
+					Toast.LENGTH_LONG).show();
+		} else {
+			Toast.makeText(getApplicationContext(), "Already on",
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	public void off(View v) {
+		bluetoothAdapter.disable();
+		Toast.makeText(getApplicationContext(), "Turned off", Toast.LENGTH_LONG)
+				.show();
 	}
 
 	@Override
@@ -36,6 +55,11 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public  void visible(View v){
+	      Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+	      startActivityForResult(getVisible, 0);
+	}	   
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
